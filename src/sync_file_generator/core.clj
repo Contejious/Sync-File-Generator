4e79a2e52b139b3fcafadf9410b7b16c629c2867
contejious <tejgandham@gmail.com>
2016-04-20 15:58:18 -0400

    first commit

---
+++ src/sync_file_generator/core.clj
@@ -0,0 +1,16 @@
+(ns sync-file-generator.core)
+
+(def headers ["CustomID"
+              "First Name"
+              "Last Name"
+              "Title"
+              "Site"
+              "Dept"
+              "Email"
+              "Active"
+              "PolicyTech"
+              "Active"])
+
+
+
+
---
+++ test/sync_file_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns sync-file-generator.core-test
+  (:require [clojure.test :refer :all]
+            [sync-file-generator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
